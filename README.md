7204d6a95fe0d682b1f8007de1779c739814a17a
Arnau <arnau@ersilia.io>
2025-09-23 11:01:12 +0200

    creating scripts

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ersilia-models-chembl-irb
 Running Ersilia models on ChEMBL v.36 compounds at the IRB cluster
+
+## Prerequisites
+
+The ChEMBL database must be installed locally before running any script in this repository. You can follow the installation guidelines described in the [chembl-antimicrobial-tasks](https://github.com/ersilia-os/chembl-antimicrobial-tasks/tree/main) repository.
+
+## Steps
+
+1. Getting compounds with reported bioactivity on the ChEMBL database. Splitting compounds in batches of 10,000. 
---
+++ scripts/1_get_ChEMBL_compounds.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import psycopg
+import os
+
+
+# root = os.path.dirname(os.path.abspath(__file__))
+root = "."
+
+def export_query(conn, sql: str, outfile: str) -> None:
+    os.makedirs(os.path.dirname(outfile), exist_ok=True)
+    print(f"Exporting query -> {outfile}")
+    with conn.cursor() as cur, open(outfile, "w", encoding="utf-8", newline="") as f:
+        with cur.copy(f"COPY ({sql}) TO STDOUT WITH (FORMAT csv, HEADER true)") as copy:
+            for data in copy:
+                f.write(data.tobytes().decode("utf-8"))
+
+def export_chembl_structures(conn, outfile):
+    sql = """
+    SELECT md.chembl_id,
+           cs.standard_inchi_key,
+           cs.canonical_smiles
+    FROM molecule_dictionary md
+    JOIN compound_structures cs
+      ON md.molregno = cs.molregno
+    ORDER BY cs.standard_inchi_key
+    """
+    export_query(conn, sql, outfile)
+
+
+# Define output file
+outfile = os.path.join(root, "..", "data", "ChEMBL_v36.csv")
+
+# Run query
+with psycopg.connect(
+    dbname="chembl_36",
+    user="chembl_user",
+    password="aaa",
+    host="localhost",
+    port=5432,
+) as conn:
+    export_chembl_structures(conn, outfile)
+
+# Reading ChEMBL data
+ChEMBL_data = pd.read_csv(outfile)
+
+# Creating splits
+split_size = 10000
+ChEMBL_data["split"] = (ChEMBL_data.index // split_size).astype(str).str.zfill(3)
+
+# Get splits
+splits = sorted(set(ChEMBL_data['split']))
+
+print(f"Splitting compounds - split_size: {split_size}")
+
+# Create splits directory
+os.makedirs(os.path.join(root, "..", "data", "splits"), exist_ok=True)
+
+# For each split
+for split in splits:
+
+    # Create split file
+    df = pd.DataFrame()
+    df['smiles'] = ChEMBL_data[ChEMBL_data['split'] == split]['canonical_smiles'].tolist()
+    df.to_csv(os.path.join(root, "..", "data", "splits", f"ChEMBL_{split}.csv"), index=False)
